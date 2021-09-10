f6d075f308f3ba9a7f59272a86b96439455f68fe
F-Uzun <1431024@dogus.edu.tr>
2021-09-10 19:04:55 +0300

    5.

--- RaceCars/Assets/Standard Assets/Vehicles/Car/Scripts/SaveScript.cs
+++ RaceCars/Assets/Standard Assets/Vehicles/Car/Scripts/SaveScript.cs
@@ -8,6 +8,25 @@ public class SaveScript : MonoBehaviour
     public static float Speed;
     public static float TopSpeed;
     public static int Gear;
+    public static int LapNumber;
+    public static bool LapChange = false;
+    public static float LapTimeMinutes;
+    public static float LapTimeSeconds;
+    public static float RaceTimeMinutes;
+    public static float RaceTimeSeconds;
+    public static float BestLapTimeM;
+    public static float BestLapTimeS;
+    public static float LastLapM;
+    public static float LastLapS;
+    public static float GameTime;
+    public static float LastCheckPoint1;
+    public static float ThisCheckPoint1;
+    public static float LastCheckPoint2;
+    public static float ThisCheckPoint2;
+    public static bool CheckPointPass1 = false;
+    public static bool CheckPointPass2 = false;
+
+
     void Start()
     {
         
@@ -15,6 +34,33 @@ public class SaveScript : MonoBehaviour
 
     void Update()
     {
-        
+        if (LapChange == true)
+        {
+            LapChange = false;
+            LapTimeMinutes = 0f;
+            LapTimeSeconds = 0f;
+            GameTime = 0f;
+        }
+
+        if (LapNumber >= 1)
+        {
+            LapTimeSeconds = LapTimeSeconds + 1 * Time.deltaTime;
+            RaceTimeSeconds = RaceTimeSeconds + 1 * Time.deltaTime;
+            GameTime = GameTime + 1 * Time.deltaTime;
+        }
+
+        if(LapTimeSeconds > 59)
+        {
+            LapTimeSeconds = 0f;
+            LapTimeMinutes++;
+        }
+
+        if (RaceTimeSeconds > 59)
+        {
+            RaceTimeSeconds = 0f;
+            RaceTimeMinutes++;
+        }
+
+
     }
 }
--- RaceCars/Assets/Standard Assets/Vehicles/Car/Scripts/UIScript.cs
+++ RaceCars/Assets/Standard Assets/Vehicles/Car/Scripts/UIScript.cs
@@ -8,7 +8,19 @@ public class UIScript : MonoBehaviour
     public Image SpeedRing;
     public Text SpeedText;
     public Text GearText;
+    public Text LapNumberText;
+    public Text TotalLapText;
+    public Text LapTimeMinutesText;
+    public Text LapTimeSecondsText;
+    public Text RaceTimeMinutesText;
+    public Text RaceTimeSecondsText;
+    public Text BestLapTimeMinutes;
+    public Text BestLapTimeSeconds;
+    public Text CheckPointTime;
+    public GameObject CheckPointDisplay;
     private float DisplaySpeed;
+    public int TotalLaps = 3;
+    
     
 
     void Start()
@@ -16,13 +28,153 @@ public class UIScript : MonoBehaviour
         SpeedRing.fillAmount = 0;
         SpeedText.text = "0";
         GearText.text = "1";
+        LapNumberText.text = "0";
+        TotalLapText.text = "/ " + TotalLaps.ToString();
+        CheckPointDisplay.SetActive(false);
     }
 
     void Update()
     {
+        //Hız göstergesi
         DisplaySpeed = SaveScript.Speed / SaveScript.TopSpeed;
         SpeedRing.fillAmount = DisplaySpeed;
         SpeedText.text = (Mathf.Round(SaveScript.Speed).ToString());
         GearText.text = (SaveScript.Gear+1).ToString();
+
+        //Lap number
+        LapNumberText.text = SaveScript.LapNumber.ToString();
+
+        //LapTime
+        if(SaveScript.LapTimeMinutes <= 9)
+        {
+            LapTimeMinutesText.text = "0" + (Mathf.Round(SaveScript.LapTimeMinutes).ToString()) + ":";
+        }
+
+        else if (SaveScript.LapTimeMinutes >= 10)
+        {
+            LapTimeMinutesText.text =(Mathf.Round(SaveScript.LapTimeMinutes).ToString()) + ":";
+        }
+
+        if (SaveScript.LapTimeSeconds <= 9)
+        {
+            LapTimeSecondsText.text = "0" + (Mathf.Round(SaveScript.LapTimeSeconds).ToString());
+        }
+
+        else if (SaveScript.LapTimeSeconds >= 10)
+        {
+            LapTimeSecondsText.text = (Mathf.Round(SaveScript.LapTimeSeconds).ToString());
+        }
+
+
+        //RaceTime
+        if (SaveScript.RaceTimeMinutes <= 9)
+        {
+            RaceTimeMinutesText.text = "0" + (Mathf.Round(SaveScript.RaceTimeMinutes).ToString()) + ":";
+        }
+
+        else if (SaveScript.RaceTimeMinutes >= 10)
+        {
+            RaceTimeMinutesText.text = (Mathf.Round(SaveScript.RaceTimeMinutes).ToString()) + ":";
+        }
+
+        if (SaveScript.LapTimeSeconds <= 9)
+        {
+            RaceTimeSecondsText.text =(Mathf.Round(SaveScript.RaceTimeSeconds).ToString());
+        }
+
+        else if (SaveScript.LapTimeSeconds >= 10)
+        {
+            RaceTimeSecondsText.text = (Mathf.Round(SaveScript.RaceTimeSeconds).ToString());
+        }
+
+        //BestLapTime
+        if(SaveScript.LastLapM == SaveScript.BestLapTimeM)
+        {
+            if (SaveScript.LastLapS < SaveScript.BestLapTimeS)
+            {
+                SaveScript.BestLapTimeS = SaveScript.LastLapS;
+            }
+        }
+
+        if (SaveScript.LastLapM == SaveScript.BestLapTimeM)
+        {
+            SaveScript.BestLapTimeM = SaveScript.LastLapM;
+            SaveScript.BestLapTimeS = SaveScript.LastLapS;
+        }
+
+
+        //Display Best Lap Time
+        if (SaveScript.BestLapTimeM <= 9)
+        {
+            BestLapTimeMinutes.text = "0" + (Mathf.Round(SaveScript.BestLapTimeM).ToString()) + ":";
+        }
+
+        else if (SaveScript.BestLapTimeM >= 10)
+        {
+            BestLapTimeMinutes.text = (Mathf.Round(SaveScript.BestLapTimeM).ToString()) + ":";
+        }
+
+        if (SaveScript.BestLapTimeS <= 9)
+        {
+            BestLapTimeSeconds.text = (Mathf.Round(SaveScript.BestLapTimeS).ToString());
+        }
+
+        else if (SaveScript.BestLapTimeS >= 10)
+        {
+            BestLapTimeSeconds.text = (Mathf.Round(SaveScript.BestLapTimeS).ToString());
+        }
+
+
+        //checkpoint1
+
+        if (SaveScript.CheckPointPass1 == true)
+        {
+            SaveScript.CheckPointPass1 = false;
+            CheckPointDisplay.SetActive(true);
+
+            if(SaveScript.ThisCheckPoint1 > SaveScript.LastCheckPoint1)
+            {
+                CheckPointTime.color = Color.red;
+                CheckPointTime.text = "-" + (SaveScript.ThisCheckPoint1 - SaveScript.LastCheckPoint1).ToString();
+                StartCoroutine(CheckPointOff());
+            }
+
+            if (SaveScript.ThisCheckPoint1 < SaveScript.LastCheckPoint1)
+            {
+                CheckPointTime.color = Color.green;
+                CheckPointTime.text = "+" + (SaveScript.LastCheckPoint1 - SaveScript.ThisCheckPoint1).ToString();
+                StartCoroutine(CheckPointOff());
+            }
+        }
+
+
+        //checkpoint2
+
+        if (SaveScript.CheckPointPass2 == true)
+        {
+            SaveScript.CheckPointPass2 = false;
+            CheckPointDisplay.SetActive(true);
+
+            if (SaveScript.ThisCheckPoint2 > SaveScript.LastCheckPoint2)
+            {
+                CheckPointTime.color = Color.red;
+                CheckPointTime.text = "-" + (SaveScript.ThisCheckPoint2 - SaveScript.LastCheckPoint2).ToString();
+                StartCoroutine(CheckPointOff());
+            }
+
+            if (SaveScript.ThisCheckPoint2 < SaveScript.LastCheckPoint2)
+            {
+                CheckPointTime.color = Color.green;
+                CheckPointTime.text = "+" + (SaveScript.LastCheckPoint2 - SaveScript.ThisCheckPoint2).ToString();
+                StartCoroutine(CheckPointOff());
+            }
+        }
+
+    }
+
+    IEnumerator CheckPointOff()
+    {
+        yield return new WaitForSeconds(2);
+        CheckPointDisplay.SetActive(false);
     }
 }
