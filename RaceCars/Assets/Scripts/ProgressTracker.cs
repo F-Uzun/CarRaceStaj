fe163df7c76c7132119683888e44fc06f7179c31
F-Uzun <1431024@dogus.edu.tr>
2021-09-13 19:06:00 +0300

    6.

---
+++ RaceCars/Assets/Scripts/ProgressTracker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressTracker : MonoBehaviour
+{
+    private AudioSource Player;
+    private bool IsPlaying = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Barrier"))
+        {
+            if (IsPlaying == false)
+            {
+                IsPlaying = true;
+                Player.Play();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Barrier"))
+        {
+            if (IsPlaying == true)
+            {
+                IsPlaying = false;
+            }
+        }
+    }
+}
